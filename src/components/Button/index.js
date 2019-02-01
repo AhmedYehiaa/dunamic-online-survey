58545edbd1bab1e767170af36e118de541f9f846
Yehia <ahmed.yehia@cognitev.com>
2019-02-01 18:06:11 +0200

    Add button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,46 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const TYPES = {
+  PRIMARY: 'primary',
+  WARNING: 'warning',
+  DANGER: 'danger',
+  SUCCESS: 'success',
+};
+
+export const SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large',
+};
+
+const Button = ({
+  type, text, disabled, buttonType, buttonSize, onClick,
+}) => (
+  <button
+    type={type}
+    className={`button ${buttonType} ${buttonSize}`}
+    disabled={disabled}
+    onClick={onClick}
+  >
+    {text}
+  </button>
+);
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  buttonType: PropTypes.string,
+  buttonSize: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  buttonType: TYPES.PRIMARY,
+  buttonSize: SIZES.MEDIUM,
+  disabled: false,
+};
+
+export default Button;
