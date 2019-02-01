85495685d479fee43c815684373ac7e86fc56614
Yehia <ahmed.yehia@cognitev.com>
2019-02-01 18:49:58 +0200

    Add Filler component

---
+++ src/components/Filler/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Filler = (props) => {
+  const { percentage } = props;
+  return (
+    <div className="filler" style={{ width: `${percentage}%` }}>
+      <span>
+        {percentage}
+        %
+      </span>
+    </div>
+  );
+};
+
+Filler.propTypes = {
+  percentage: PropTypes.number.isRequired,
+};
+export default Filler;
