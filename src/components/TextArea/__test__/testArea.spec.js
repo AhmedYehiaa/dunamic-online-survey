e7ed64b71414743a0bcf0af205e2d2686392068c
Yehia <ahmed.yehia@cognitev.com>
2019-02-03 12:47:48 +0200

    Add tests to textArea component

---
+++ src/components/TextArea/__test__/testArea.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TextArea from '../index';
+
+test('it should render textArea component with ', () => {
+  const onChange = jest.fn();
+  const component = renderer.create(
+    <TextArea
+      className="text-area"
+      value="my answer"
+      placeholder="please enter your feedback"
+      onChange={e => onChange(e.target.value)}
+    />,
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
