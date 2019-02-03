8d9352a5613c16882b1697f3b5892463b0deefbb
Yehia <ahmed.yehia@cognitev.com>
2019-02-03 13:37:31 +0200

    Add Enzyme configuration

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
-  clear: jest.fn()
+  clear: jest.fn(),
 };
+
 global.localStorage = localStorageMock;
+global.shallow = shallow;
