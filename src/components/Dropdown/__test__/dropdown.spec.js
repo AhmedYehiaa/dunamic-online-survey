57864130deb81c75965d37f7937dec553912be20
Yehia <ahmed.yehia@cognitev.com>
2019-02-03 14:14:16 +0200

    Add dropdown component test

---
+++ src/components/Dropdown/__test__/dropdown.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Dropdown from '../index';
+
+it('it should renders the button', () => {
+  const handleUpdatingAnswer = jest.fn();
+  const options = [{
+    name: 'name1',
+    value: 'value1',
+  }];
+  const wrapper = shallow(
+    <Dropdown
+      options={options}
+      value="value1"
+      onChange={handleUpdatingAnswer}
+    />,
+  );
+  expect(wrapper.children.length).toEqual(1);
+  expect(wrapper).toMatchSnapshot();
+});
