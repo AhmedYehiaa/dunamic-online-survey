25930aafbbcc01802c7b2a4086b49acf1334902b
Yehia <ahmed.yehia@cognitev.com>
2019-02-03 09:16:53 +0200

    Update radioOption component

--- src/components/RadioOption/index.js
+++ src/components/RadioOption/index.js
@@ -5,20 +5,32 @@ import PropTypes from 'prop-types';
 const RadioOption = ({
   name,
   value,
+  defaultChecked,
   label,
-  handleInputChange,
+  onClick,
 }) => (
   <p className="radio-option">
-    <input type="radio" id={value} name={name} onClick={() => handleInputChange(value)} />
+    <input
+      type="radio"
+      name={name}
+      id={value}
+      defaultChecked={defaultChecked}
+      onClick={() => onClick(value)}
+    />
     <label htmlFor={value}>{label}</label>
   </p>
 );
 
 RadioOption.propTypes = {
   name: PropTypes.string.isRequired,
+  defaultChecked: PropTypes.bool,
   value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  handleInputChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+RadioOption.defaultProps = {
+  defaultChecked: false,
 };
 
 export default RadioOption;
